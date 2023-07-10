1565f85003e3cf46e1ee8314b83766baea7a2ebc
joshmorrison04 <54602588+joshmorrison04@users.noreply.github.com>
2023-07-10 13:37:14 -0700

    Add files via upload

---
+++ Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return ( 
+        <div className="post">
+        <div className="image">
+          <img src="https://cdn.vox-cdn.com/thumbor/r6-fTFPUYhOAtxjxU7RgittkfQc=/0x0:7360x4912/1820x1213/filters:focal(1120x1015:2296x2191):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/72411858/1230576984.0.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>AI is supposedly the new nuclear weapons — but how similar are they, really?</h2>
+         <p className = "info">
+           <a className="author">Joshua Morrison</a>
+           <time>2023-06-07</time>
+         </p>
+          <p className="Summary">What the history of nuclear arms can — and can’t — tell us about the future of AI.</p>
+        </div>
+      </div>
+    );
+}
